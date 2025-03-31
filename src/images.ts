2340273587f0923106819c351f957d065cf1dd6e
Taranovskaya <s5198653@yandex.ru>
2025-03-31 16:22:25 +0300

    adds images and types for it

---
+++ src/images.ts
@@ -0,0 +1,40 @@
+import type { Card } from '@/types.ts';
+
+export const images: Card[] = [
+  {
+    name: 'cat',
+    image: '/src/components/icons/cat.svg',
+  },
+  {
+    name: 'chicken',
+    image: '/src/components/icons/chicken.svg',
+  },
+  {
+    name: 'dog',
+    image: '/src/components/icons/dog.svg',
+  },
+  {
+    name: 'frog',
+    image: '/src/components/icons/frog.svg',
+  },
+  {
+    name: 'monkey',
+    image: '/src/components/icons/monkey.svg',
+  },
+  {
+    name: 'parrot',
+    image: '/src/components/icons/parrot.svg',
+  },
+  {
+    name: 'penguin',
+    image: '/src/components/icons/penguin.svg',
+  },
+  {
+    name: 'rabbit',
+    image: '/src/components/icons/rabbit.svg',
+  },
+  {
+    name: 'turtle',
+    image: '/src/components/icons/turtle.svg',
+  },
+];
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export type Card = {
+  name: string;
+  image: string;
+};
