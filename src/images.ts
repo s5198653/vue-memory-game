7807d911aab7cbd3ef4ac67dbb4286b559c52d76
Taranovskaya <s5198653@yandex.ru>
2025-04-21 12:39:38 +0300

    move images to assets folder

--- src/images.ts
+++ src/images.ts
@@ -3,38 +3,38 @@ import type { Card } from '@/types.ts';
 export const images: Card[] = [
   {
     name: 'cat',
-    image: '/src/components/icons/cat.svg',
+    image: '/src/assets/icons/cat.svg',
   },
   {
     name: 'chicken',
-    image: '/src/components/icons/chicken.svg',
+    image: '/src/assets/icons/chicken.svg',
   },
   {
     name: 'dog',
-    image: '/src/components/icons/dog.svg',
+    image: '/src/assets/icons/dog.svg',
   },
   {
     name: 'frog',
-    image: '/src/components/icons/frog.svg',
+    image: '/src/assets/icons/frog.svg',
   },
   {
     name: 'monkey',
-    image: '/src/components/icons/monkey.svg',
+    image: '/src/assets/icons/monkey.svg',
   },
   {
     name: 'parrot',
-    image: '/src/components/icons/parrot.svg',
+    image: '/src/assets/icons/parrot.svg',
   },
   {
     name: 'penguin',
-    image: '/src/components/icons/penguin.svg',
+    image: '/src/assets/icons/penguin.svg',
   },
   {
     name: 'rabbit',
-    image: '/src/components/icons/rabbit.svg',
+    image: '/src/assets/icons/rabbit.svg',
   },
   {
     name: 'turtle',
-    image: '/src/components/icons/turtle.svg',
+    image: '/src/assets/icons/turtle.svg',
   },
 ];
